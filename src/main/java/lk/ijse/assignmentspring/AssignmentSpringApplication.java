fe64e8a0ed7c9fe616db31e19ce6e1e1c80d07eb
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-09-01 10:31:52 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/assignmentspring/AssignmentSpringApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.assignmentspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/assignmentspring/AssignmentSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.assignmentspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
